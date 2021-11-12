ddaf11ec680a9d65c6772817abaacbfa9720042c
Daniel Haden <haden.daniel@gmail.com>
2021-11-12 01:23:38 -0700

    Added MultiRegionView projects and updated names

---
+++ MultiRegionView.Core/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MvvmCross.Core;
+using MvvmCross.Platforms.Wpf.Core;
+using MvvmCross.Platforms.Wpf.Views;
+
+namespace MultiRegionView.Core
+{
+    public partial class App : MvxApplication
+    {
+        protected override void RegisterSetup()
+        {
+            // register the setup class
+            this.RegisterSetupType<Setup>();
+        }
+    }
+}
---
+++ MultiRegionView.WPF/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MvvmCross.Platforms.Wpf.Views;
+
+namespace MultiRegionView.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MvxWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
